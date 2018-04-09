c198430cbe99bc4ed4c108786ae5682fd7c72e09
munirkizilcali <munir.kizilcali@gmail.com>
2018-04-09 12:50:29 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_data = {}
+  data.each do |feat, values|
+    values.each do |sub_feat, pigeons|
+      pigeons.each do |pigeon|
+        
+        if !new_data.has_key?(pigeon)
+          new_data[pigeon] = {}
+        end  
+        if !new_data[pigeon].has_key?(feat)
+          new_data[pigeon][feat] = []
+          #binding.pry
+        end
+        new_data[pigeon][feat] << sub_feat.to_s
+      end
+    end
+  end
+  new_data
 end
